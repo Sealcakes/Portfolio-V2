4cc71a47dae9766dc13cc1eec4050d1d78b9c033
Sealcakes <taylorcseal@gmail.com>
2023-04-21 14:37:06 -0500

    adds more file structure, basic page layout and begins work on main nav element

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import MainNav from './MainNav';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <MainNav currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+};
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Portfolio() {
+    return (
+        <div>
+            <h1>My Portfolio Page</h1>
+        </div>
+    );
+};
