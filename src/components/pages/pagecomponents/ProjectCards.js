3092ae5e73f137dd4baf1cd481f4bb7b29390242
Sealcakes <taylorcseal@gmail.com>
2023-04-25 17:18:08 -0500

    adds close button to expanded cards

---
+++ src/components/pages/pagecomponents/ProjectCards.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const projects = [
+    {
+        id: 1,
+        title: "Test1 Title",
+        subtitle: "Brief Description",
+        description: "This is a longer version of the project description, it used a lot of things, it's purpose is to pass the butter",
+    },
+    {
+        id: 2,
+        title: "Test2 Title",
+        subtitle: "Another Brief Description",
+        description: "Another longer version of the second project's description, it also used a lot of things."
+    },
+    {
+        id: 3,
+        title: "Test2 Title",
+        subtitle: "Another Brief Description",
+        description: "Another longer version of the second project's description, it also used a lot of things."
+    },
+    {
+        id: 4,
+        title: "Test2 Title",
+        subtitle: "Another Brief Description",
+        description: "Another longer version of the second project's description, it also used a lot of things."
+    },
+    {
+        id: 5,
+        title: "Test2 Title",
+        subtitle: "Another Brief Description",
+        description: "Another longer version of the second project's description, it also used a lot of things."
+    },
+]
+
+export default function ProjectCards() {
+
+    const [cardId, setCardId] = useState(null);
+
+    const handleProjectCardClick = (projectId) => {
+        setCardId(projectId);
+    }
+
+    const handleCloseProjectCardClick = () => {
+        setCardId(null);
+    }
+    
+    const renderProjectCards = () => 
+        projects.map((project) => (
+            <motion.div
+                key={project.id}
+                className='project-card'
+                layoutId={project.id}
+                onClick={() => handleProjectCardClick(project.id)}
+            >
+                <motion.h2>{project.title}</motion.h2>
+                <motion.h5>{project.subtitle}</motion.h5>
+            </motion.div>
+        ));
+
+    const renderSelectedProjectCard = () => {
+        const selectedProject = projects.find((project) => project.id === cardId);
+        return (
+            <motion.div
+                key={selectedProject.id}
+                className='project-card expanded'
+                layoutId={selectedProject.id}
+                animate={{x: '25%', y: '25%'}}
+            >
+                <motion.span className='card-close-button' onClick={() => handleCloseProjectCardClick()}>+</motion.span>
+                <motion.h2>{selectedProject.title}</motion.h2>
+                <motion.h4>{selectedProject.subtitle}</motion.h4>
+                <motion.p>{selectedProject.description}</motion.p>
+            </motion.div>
+        )
+    }
+
+    return (
+        <div className='project-cards-container'>
+            {renderProjectCards()}
+            <AnimatePresence>
+                {cardId && renderSelectedProjectCard()}
+            </AnimatePresence>
+        </div>
+    );
+};
